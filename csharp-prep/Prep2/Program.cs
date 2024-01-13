977e5219adfdb46ff6fe7127bf7530793ea1cf76
Jackson Westover <wes19002@byui.edu>
2024-01-12 17:06:52 -0700

    Letter grades

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,5 +5,47 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Enter your score: ");
+        float score = float.Parse(Console.ReadLine());
+
+        string letter;
+        string status;
+
+        if (score >= 90)
+        {
+            letter = "A";
+        }
+        else if (score >= 80)
+        {
+            letter = "B";
+        }
+        else if (score >= 70)
+        {
+            letter = "C";
+        }
+        else if (score >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        };
+
+        Console.WriteLine($"Your grade is: {letter}");
+
+        if (score >= 70)
+        {
+            Console.WriteLine("Congratulations you passed!");
+        }
+        else
+        {
+            Console.WriteLine("You failed. Try again.");
+        }
+
+
+
+
+
     }
 }
