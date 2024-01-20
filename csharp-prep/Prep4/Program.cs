249e3fed252cd9f5bb258ca290604549e5b0c6e3
Jackson Westover <wes19002@byui.edu>
2024-01-20 15:33:51 -0700

    Prep4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+
+        int newNumber = -1;
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        while (newNumber != 0)
+        {
+            Console.Write("Enter number: ");
+            newNumber = int.Parse(Console.ReadLine());
+
+            if (newNumber != 0)
+            {
+                numbers.Add(newNumber);
+            }
+        }
+
+        // Finding sum
+        int sum = 0;
+        foreach (int value in numbers)
+        {
+            sum += value;
+        }
+
+        // Finding Average
+        float average = ((float)sum) / numbers.Count;
+
+        // Finding max
+        int largestNum = 0;
+        foreach (int value in numbers)
+        {
+            if (value > largestNum)
+            {
+                largestNum = value;
+            }
+        }
+
+        // Displaying the stats
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {largestNum}");
     }
 }
