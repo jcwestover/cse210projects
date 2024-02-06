c98c428fe3b3fb70b575ba3d5bacf6c4ab467165
Jackson Westover <wes19002@byui.edu>
2024-02-06 13:40:55 -0700

    fraction program complete

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -8,5 +8,14 @@ class Program
         Console.WriteLine(f1.GetFractionString());
         Console.WriteLine(f1.GetDecimalValue());
 
+        Fraction f2 = new Fraction(5);
+        Console.WriteLine(f2.GetFractionString());
+        Console.WriteLine(f2.GetDecimalValue());
+
+        Fraction f3 = new Fraction(2, 4);
+        Console.WriteLine(f3.GetFractionString());
+        Console.WriteLine(f3.GetDecimalValue());
+
+
     }
 }
