70ed489384b7fa40f7c95a7d01e7842f3afd9b6e
Jackson Westover <wes19002@byui.edu>
2024-02-27 13:38:56 -0700

    spinner fixed

--- prove/Develop04/Activity.cs
+++ prove/Develop04/Activity.cs
@@ -6,4 +6,42 @@ public class Activity
     {
         "\\", "|", "/","-"
     };
+
+    public string BeginningMessage()
+    {
+        Console.WriteLine($"Welcome to the {_name}.");
+    }
+
+    public string Spinner()
+    {   
+        int i = 0;
+
+        System.Diagnostics.Stopwatch s = new System.Diagnostics.Stopwatch();
+        s.Start();
+        while (s.Elapsed < TimeSpan.FromSeconds(2)) 
+        {            
+            string line = _spinnerList[i];
+            Console.Write(line);
+            Thread.Sleep(375);
+            Console.Write("\b \b");
+
+            i++;
+
+            if (i >= _spinnerList.Length)
+            {
+                i = 0;
+            }
+            
+        }
+
+        s.Stop();
+    }
+
+     public string EndingMessage()
+     {
+        Console.WriteLine($"Well done!")
+
+     }
+
+
 }
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,9 +1,39 @@
 using System;
+System.Diagnostics.Stopwatch StopWatch = new System.Diagnostics.Stopwatch();
 
-class Program
-{
-    static void Main(string[] args)
+int _length = 10;
+string[] _spinnerList = new string []
     {
-        Console.WriteLine("Hello Sandbox World!");
-    }
-}
+        "\\", "|", "/","-"
+    };
+
+Console.WriteLine("Get ready...");
+        
+        int i = 0;
+
+
+        System.Diagnostics.Stopwatch s = new System.Diagnostics.Stopwatch();
+        s.Start();
+        while (s.Elapsed < TimeSpan.FromSeconds(_length)) 
+        {            
+            string line = _spinnerList[i];
+            Console.Write(line);
+            Thread.Sleep(375);
+            Console.Write("\b \b");
+
+            i++;
+
+            if (i >= _spinnerList.Length)
+            {
+                i = 0;
+            }
+            
+            // Console.Write("+");
+
+            // Thread.Sleep(500);
+
+            // Console.Write("\b \b"); // Erase the + character
+            // Console.Write("-"); // Replace it with the - character
+        }
+
+        s.Stop();
