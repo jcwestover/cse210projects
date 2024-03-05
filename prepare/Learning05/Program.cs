fcde80e760e8714d99a7b8ecfc48b81b708760ef
Jackson Westover <wes19002@byui.edu>
2024-03-05 13:38:23 -0700

    Final

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,24 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Square mySquare = new Square("Blue", 5);
+        List<Shape> shapes = new List<Shape>();
 
-        Console.WriteLine(mySquare.Color);
-        Console.WriteLine(mySquare.GetArea());
+        Square mySquare = new Square("Blue", 5);
+        shapes.Add(mySquare);
+
+
+        // Console.WriteLine(mySquare.Color);
+        // Console.WriteLine(mySquare.GetArea());
 
         Triangle myTri = new Triangle("Red", 5, 6);
+        shapes.Add(myTri);
 
-        Console.WriteLine(myTri.Color);
-        Console.WriteLine(myTri.GetArea());
+        // Console.WriteLine(myTri.Color);
+        // Console.WriteLine(myTri.GetArea());
 
         Rectangle myRec = new Rectangle("Green", 7, 6);
+        shapes.Add(myRec);
 
-        Console.WriteLine(myRec.Color);
-        Console.WriteLine(myRec.GetArea());
+        // Console.WriteLine(myRec.Color);
+        // Console.WriteLine(myRec.GetArea());
 
         Circle myCir = new Circle("Purple", 5);
+        shapes.Add(myCir);
 
-        Console.WriteLine(myCir.Color);
-        Console.WriteLine(myCir.GetArea());
+        // Console.WriteLine(myCir.Color);
+        // Console.WriteLine(myCir.GetArea());
+
+        foreach (Shape s in shapes)
+        {
+            string color = s.Color;
+
+            double area = s.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area}");
+
+        }
     }
 }
